b3f21c521a5f4ff5a9975237e4ff023f5f1dd5cd
LeslT <lesliejailine@hotmail.com>
2024-03-19 18:47:10 -0500

    last steps

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ function Header({ cart, removeFromCart, clearCart ,increaseQuantity, decreaseQua
               <a href="index.html">
                 <img
                   className="img-fluid"
-                  src="./public/img/logo.svg"
+                  src="/img/logo.svg"
                   alt="imagen logo"
                 />
               </a>
@@ -22,7 +22,7 @@ function Header({ cart, removeFromCart, clearCart ,increaseQuantity, decreaseQua
               <div className="carrito">
                 <img
                   className="img-fluid"
-                  src="./public/img/carrito.png"
+                  src="/img/carrito.png"
                   alt="imagen carrito"
                 />
 
